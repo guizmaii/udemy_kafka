e118ecb6be2f9f277f172d343b242ddfa68fa972
jules Ivanic <jules.ivanic@gmail.com>
2019-10-21 16:42:19 +1100

    clean script, BIS

--- 1 - Kafka Streams for Data Processing/code/1-course-intro/course-intro-mac-linux.sh
+++ 1 - Kafka Streams for Data Processing/code/1-course-intro/course-intro-mac-linux.sh
@@ -6,19 +6,19 @@
 ### LINUX / MAC OS X ONLY
 
 # open a shell - zookeeper is at localhost:2181
-zookeeper-server-start.sh config/zookeeper.properties
+zookeeper-server-start config/zookeeper.properties
 
 # open another shell - kafka is at localhost:9092
-kafka-server-start.sh config/server.properties
+kafka-server-start config/server.properties
 
 # create input topic
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-plaintext-input
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-plaintext-input
 
 # create output topic
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-wordcount-output
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-wordcount-output
 
 # start a kafka producer
-kafka-console-producer.sh --broker-list localhost:9092 --topic streams-plaintext-input
+kafka-console-producer --broker-list localhost:9092 --topic streams-plaintext-input
 # enter
 kafka streams udemy
 kafka data processing
@@ -26,10 +26,10 @@ kafka streams course
 # exit
 
 # verify the data has been written
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic streams-plaintext-input --from-beginning
+kafka-console-consumer --bootstrap-server localhost:9092 --topic streams-plaintext-input --from-beginning
 
 # start a consumer on the output topic
-kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+kafka-console-consumer --bootstrap-server localhost:9092 \
     --topic streams-wordcount-output \
     --from-beginning \
     --formatter kafka.tools.DefaultMessageFormatter \
@@ -39,6 +39,6 @@ kafka-console-consumer.sh --bootstrap-server localhost:9092 \
     --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
 
 # start the streams application
-kafka-run-class.sh org.apache.kafka.streams.examples.wordcount.WordCountDemo
+kafka-run-class org.apache.kafka.streams.examples.wordcount.WordCountDemo
 
 # verify the data has been written to the output topic!
