01d7e3baff70154fb0bca5cd6e87930f601a8067
jules Ivanic <jules.ivanic@gmail.com>
2019-10-24 16:34:50 +1100

    Clean script

--- 1 - Kafka Streams for Data Processing/3-favourite-colour/favourite-colour.sh
+++ 1 - Kafka Streams for Data Processing/3-favourite-colour/favourite-colour.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 
 # create input topic with one partition to get full ordering
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic favourite-colour-input
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic favourite-colour-input
 
 # create intermediary log compacted topic
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic user-keys-and-colours --config cleanup.policy=compact
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic user-keys-and-colours --config cleanup.policy=compact
 
 # create output log compacted topic
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic favourite-colour-output --config cleanup.policy=compact
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic favourite-colour-output --config cleanup.policy=compact
 
 
 # launch a Kafka consumer
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+kafka-console-consumer --bootstrap-server localhost:9092 \
     --topic favourite-colour-output \
     --from-beginning \
     --formatter kafka.tools.DefaultMessageFormatter \
@@ -23,7 +23,7 @@ bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
 # launch the streams application
 
 # then produce data to it
-bin/kafka-console-producer.sh --broker-list localhost:9092 --topic favourite-colour-input
+kafka-console-producer --broker-list localhost:9092 --topic favourite-colour-input
 #
 stephane,blue
 john,green
@@ -32,4 +32,4 @@ alice,red
 
 
 # list all topics that we have in Kafka (so we can observe the internal topics)
-bin/kafka-topics.sh --list --zookeeper localhost:2181
+kafka-topics --list --zookeeper localhost:2181
