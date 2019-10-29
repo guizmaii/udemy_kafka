ef2f068af756cf30da31308c67a3fd713f43b748
jules Ivanic <jules.ivanic@gmail.com>
2019-10-29 16:14:52 +1100

    Improve the doc about the command to launch to consume the streams

--- 1 - Kafka Streams for Data Processing/4-bank-balance/bank-balance.sh
+++ 1 - Kafka Streams for Data Processing/4-bank-balance/bank-balance.sh
@@ -1,14 +1,48 @@
 #!/bin/zsh
 
-# create input topic with one partition to get full ordering
-kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bank-transactions
-
-# create output log compacted topic
-kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bank-balance-exactly-once --config cleanup.policy=compact
-
-# launch a Kafka consumer
+# Consume the "source" topic
 kafka-console-consumer --bootstrap-server localhost:9092 \
-    --topic bank-balance-exactly-once \
+    --topic bank-balance-source-topic \
+    --from-beginning \
+    --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+
+# Consume the "sum" topic
+kafka-console-consumer --bootstrap-server localhost:9092 \
+    --topic bank-balance-sum-topic \
+    --from-beginning \
+    --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
+# Consume the "count" topic
+kafka-console-consumer --bootstrap-server localhost:9092 \
+    --topic bank-balance-transactions-count-topic \
+    --from-beginning \
+    --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
+# Consume the "last updated" topic
+kafka-console-consumer --bootstrap-server localhost:9092 \
+    --topic bank-balance-latest-update-topic \
+    --from-beginning \
+    --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+
+# Consume the "final result" topic
+kafka-console-consumer --bootstrap-server localhost:9092 \
+    --topic bank-balance-final-result-topic \
     --from-beginning \
     --formatter kafka.tools.DefaultMessageFormatter \
     --property print.key=true \
