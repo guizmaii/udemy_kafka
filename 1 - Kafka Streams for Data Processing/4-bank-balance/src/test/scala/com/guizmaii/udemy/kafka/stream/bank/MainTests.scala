95c7dac9b65148411e7ceef7170b9248d2cb77d3
jules Ivanic <jules.ivanic@gmail.com>
2019-10-31 10:43:47 +1100

    Improve test code

--- 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/MainTests.scala
+++ 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/MainTests.scala
@@ -2,96 +2,33 @@ package com.guizmaii.udemy.kafka.stream.bank
 
 import java.time.Instant
 
-import cats.effect.{ IO, Resource }
-import org.apache.kafka.clients.admin.NewTopic
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.{ Deserializer, Serializer }
+import cats.effect.IO
+import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.test.ConsumerRecordFactory
-import org.apache.kafka.streams.{ Topology, TopologyTestDriver }
-import org.scalatest.{ FreeSpec, Matchers }
-
-object Helpers {
-  implicit final class ConsumerRecordFactoryOps[K, V](private val factory: ConsumerRecordFactory[K, V]) extends AnyVal {
-    def make(k: K, v: V): ConsumerRecord[Array[Byte], Array[Byte]] = factory.create(k, v)
-  }
-
-  implicit final class TopologyTestDriverOps(private val topologyTestDriver: TopologyTestDriver) extends AnyVal {
-    def read[K: Deserializer, V: Deserializer](topic: NewTopic): ProducerRecord[K, V] =
-      topologyTestDriver.readOutput(topic.name, implicitly[Deserializer[K]], implicitly[Deserializer[V]])
-  }
-}
+import org.scalatest.{FreeSpec, Matchers}
 
 class MainTests extends FreeSpec with Matchers {
 
-  import Helpers._
   import Main._
+  import TestUtils._
   import io.circe.generic.auto._
   import org.apache.kafka.streams.scala.ImplicitConversions._
   import utils.KTableOps._
   import utils.KafkaSerdesWithCirceSerdes._
 
-  def ConsumerRecordFactory[K: Serializer, V: Serializer](topic: NewTopic): ConsumerRecordFactory[K, V] =
-    new ConsumerRecordFactory[K, V](topic.name, implicitly[Serializer[K]], implicitly[Serializer[V]])
-
-  def topologyTestDriverR(topology: Topology): Resource[IO, TopologyTestDriver] =
-    Resource.fromAutoCloseable(IO.delay { new TopologyTestDriver(topology, config) })
-
   "true" - {
     "is true" in { true should be(true) }
   }
 
-  /*
-
-    - 1 input topic
-    - n input key/messages
-
-    - 1 output topic
-    - n expected records
-
-    - a topology
-
-   */
-
-  trait Producer[K, V] {
-    def produce(k: K, v: V): Unit
-  }
-
-  trait Consumer[K, V] {
-    def consume(): ProducerRecord[K, V]
-  }
-
-  def testStream[InputKey: Serializer, InputValue: Serializer, OutputKey: Deserializer, OutputValue: Deserializer, T](
-    topology: Topology
-  )(
-    inputTopic: NewTopic,
-    outputTopic: NewTopic
-  )(test: (Producer[InputKey, InputValue], Consumer[OutputKey, OutputValue]) => T): T =
-    topologyTestDriverR(topology).use { testDriver =>
-      IO.delay {
-        val factory = ConsumerRecordFactory[InputKey, InputValue](inputTopic)
-
-        val producer = new Producer[InputKey, InputValue] {
-          override def produce(k: InputKey, v: InputValue): Unit = testDriver.pipeInput(factory.make(k, v))
-        }
-        val consumer = new Consumer[OutputKey, OutputValue] {
-          override def consume(): ProducerRecord[OutputKey, OutputValue] =
-            testDriver.read[OutputKey, OutputValue](outputTopic)
-        }
-
-        test(producer, consumer)
-      }
-    }.unsafeRunSync()
-
   "Sum Stream" - {
     "sums the, grouped by key, Message amounts" in {
-      val builder = new StreamsBuilder
+      def topology(builder: StreamsBuilder): IO[Topology] =
+        for {
+          source <- sourceStream(builder)
+          _      <- sumStream(source).flatMap(_.to(sumTopic))
+        } yield builder.build()
 
-      for {
-        source <- sourceStream(builder)
-        _      <- sumStream(source).flatMap(_.to(sumTopic))
-      } yield testStream(builder.build)(sourceTopic, sumTopic) {
+      testStream(topology)(sourceTopic, sumTopic) {
         (producer: Producer[String, Message], consumer: Consumer[String, Long]) =>
           val key        = "key"
           val anotherKey = "anotherKey"
---
+++ 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/utils/TestUtils.scala
@@ -0,0 +1,82 @@
+package com.guizmaii.udemy.kafka.stream.bank
+
+import cats.effect.{ IO, Resource }
+import com.guizmaii.udemy.kafka.stream.bank.Main.config
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ Deserializer, Serializer }
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.test.ConsumerRecordFactory
+import org.apache.kafka.streams.{ Topology, TopologyTestDriver }
+
+object TestUtils {
+
+  private implicit final class ConsumerRecordFactoryOps[K, V](private val factory: ConsumerRecordFactory[K, V])
+      extends AnyVal {
+    def make(k: K, v: V): ConsumerRecord[Array[Byte], Array[Byte]] = factory.create(k, v)
+  }
+
+  private implicit final class TopologyTestDriverOps(private val topologyTestDriver: TopologyTestDriver)
+      extends AnyVal {
+    def read[K: Deserializer, V: Deserializer](topic: NewTopic): ProducerRecord[K, V] =
+      topologyTestDriver.readOutput(topic.name, implicitly[Deserializer[K]], implicitly[Deserializer[V]])
+  }
+
+  private def ConsumerRecordFactory[K: Serializer, V: Serializer](topic: NewTopic): ConsumerRecordFactory[K, V] =
+    new ConsumerRecordFactory[K, V](topic.name, implicitly[Serializer[K]], implicitly[Serializer[V]])
+
+  private def topologyTestDriverR(topology: Topology): Resource[IO, TopologyTestDriver] =
+    Resource.fromAutoCloseable(IO.delay { new TopologyTestDriver(topology, config) })
+
+  trait Producer[K, V] {
+    def produce(k: K, v: V): Unit
+  }
+
+  trait Consumer[K, V] {
+    def consume(): ProducerRecord[K, V]
+  }
+
+  private class TestStream {
+
+    val builder = new StreamsBuilder
+
+    def test[InputKey: Serializer, InputValue: Serializer, OutputKey: Deserializer, OutputValue: Deserializer, T](
+      topologyBuilder: StreamsBuilder => IO[Topology]
+    )(
+      inputTopic: NewTopic,
+      outputTopic: NewTopic
+    )(test: (Producer[InputKey, InputValue], Consumer[OutputKey, OutputValue]) => T): T = {
+      val program =
+        for {
+          topology <- topologyBuilder(builder)
+          test <- topologyTestDriverR(topology).use { testDriver =>
+                   IO.delay {
+                     val factory = ConsumerRecordFactory[InputKey, InputValue](inputTopic)
+
+                     val producer = new Producer[InputKey, InputValue] {
+                       override def produce(k: InputKey, v: InputValue): Unit = testDriver.pipeInput(factory.make(k, v))
+                     }
+                     val consumer = new Consumer[OutputKey, OutputValue] {
+                       override def consume(): ProducerRecord[OutputKey, OutputValue] =
+                         testDriver.read[OutputKey, OutputValue](outputTopic)
+                     }
+
+                     test(producer, consumer)
+                   }
+                 }
+        } yield test
+
+      program.unsafeRunSync()
+    }
+  }
+
+  def testStream[InputKey: Serializer, InputValue: Serializer, OutputKey: Deserializer, OutputValue: Deserializer, T](
+    topologyBuilder: StreamsBuilder => IO[Topology]
+  )(
+    inputTopic: NewTopic,
+    outputTopic: NewTopic
+  )(test: (Producer[InputKey, InputValue], Consumer[OutputKey, OutputValue]) => T): T =
+    (new TestStream).test(topologyBuilder)(inputTopic, outputTopic)(test)
+
+}
