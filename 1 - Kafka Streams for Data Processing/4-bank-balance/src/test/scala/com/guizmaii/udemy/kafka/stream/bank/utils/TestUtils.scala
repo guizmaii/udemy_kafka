884a6bc795396fb96c2907cecf13ae85afe600c4
jules Ivanic <jules.ivanic@gmail.com>
2019-10-31 10:53:30 +1100

    Simplify code

--- 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/utils/TestUtils.scala
+++ 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/utils/TestUtils.scala
@@ -37,46 +37,33 @@ object TestUtils {
     def consume(): ProducerRecord[K, V]
   }
 
-  private class TestStream {
-
-    val builder = new StreamsBuilder
-
-    def test[InputKey: Serializer, InputValue: Serializer, OutputKey: Deserializer, OutputValue: Deserializer, T](
-      topologyBuilder: StreamsBuilder => IO[Topology]
-    )(
-      inputTopic: NewTopic,
-      outputTopic: NewTopic
-    )(test: (Producer[InputKey, InputValue], Consumer[OutputKey, OutputValue]) => T): T = {
-      val program =
-        for {
-          topology <- topologyBuilder(builder)
-          test <- topologyTestDriverR(topology).use { testDriver =>
-                   IO.delay {
-                     val factory = ConsumerRecordFactory[InputKey, InputValue](inputTopic)
-
-                     val producer = new Producer[InputKey, InputValue] {
-                       override def produce(k: InputKey, v: InputValue): Unit = testDriver.pipeInput(factory.make(k, v))
-                     }
-                     val consumer = new Consumer[OutputKey, OutputValue] {
-                       override def consume(): ProducerRecord[OutputKey, OutputValue] =
-                         testDriver.read[OutputKey, OutputValue](outputTopic)
-                     }
-
-                     test(producer, consumer)
-                   }
-                 }
-        } yield test
-
-      program.unsafeRunSync()
-    }
-  }
-
   def testStream[InputKey: Serializer, InputValue: Serializer, OutputKey: Deserializer, OutputValue: Deserializer, T](
     topologyBuilder: StreamsBuilder => IO[Topology]
   )(
     inputTopic: NewTopic,
     outputTopic: NewTopic
-  )(test: (Producer[InputKey, InputValue], Consumer[OutputKey, OutputValue]) => T): T =
-    (new TestStream).test(topologyBuilder)(inputTopic, outputTopic)(test)
+  )(test: (Producer[InputKey, InputValue], Consumer[OutputKey, OutputValue]) => T): T = {
+    val program =
+      for {
+        topology <- topologyBuilder(new StreamsBuilder)
+        test <- topologyTestDriverR(topology).use { testDriver =>
+                 IO.delay {
+                   val factory = ConsumerRecordFactory[InputKey, InputValue](inputTopic)
+
+                   val producer = new Producer[InputKey, InputValue] {
+                     override def produce(k: InputKey, v: InputValue): Unit = testDriver.pipeInput(factory.make(k, v))
+                   }
+                   val consumer = new Consumer[OutputKey, OutputValue] {
+                     override def consume(): ProducerRecord[OutputKey, OutputValue] =
+                       testDriver.read[OutputKey, OutputValue](outputTopic)
+                   }
+
+                   test(producer, consumer)
+                 }
+               }
+      } yield test
+
+    program.unsafeRunSync()
+  }
 
 }
