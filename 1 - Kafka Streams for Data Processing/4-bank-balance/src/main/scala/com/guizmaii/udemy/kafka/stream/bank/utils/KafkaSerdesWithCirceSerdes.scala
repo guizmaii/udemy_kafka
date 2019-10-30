3daaaa1577330e607e1a0bc25fb8dddbe170dcce
jules Ivanic <jules.ivanic@gmail.com>
2019-10-31 09:39:06 +1100

    Clean

--- 1 - Kafka Streams for Data Processing/4-bank-balance/src/main/scala/com/guizmaii/udemy/kafka/stream/bank/utils/KafkaSerdesWithCirceSerdes.scala
+++ 1 - Kafka Streams for Data Processing/4-bank-balance/src/main/scala/com/guizmaii/udemy/kafka/stream/bank/utils/KafkaSerdesWithCirceSerdes.scala
@@ -13,18 +13,19 @@ import org.apache.kafka.common.serialization.{Deserializer, Serde, Serdes, Seria
  * TODO: Remove this code when this PR is merged: https://github.com/joan38/kafka-streams-circe/pull/2
  */
 trait CirceSerdes {
+  import io.circe.syntax._
 
-  implicit def serializer[T: Encoder]: Serializer[T] =
-    (_, caseClass: T) => Encoder[T].apply(caseClass).noSpaces.getBytes(StandardCharsets.UTF_8)
+  implicit final def serializer[T: Encoder]: Serializer[T] =
+    (_, t: T) => t.asJson.noSpaces.getBytes(StandardCharsets.UTF_8)
 
-  implicit def deserializer[T: Decoder]: Deserializer[T] =
+  implicit final def deserializer[T: Decoder]: Deserializer[T] =
     (_, data: Array[Byte]) =>
       if (data eq null) null.asInstanceOf[T]
       else
         decode[T](new String(data, StandardCharsets.UTF_8))
           .fold(error => throw new SerializationException(error), identity)
 
-  implicit def serde[CC: Encoder: Decoder]: Serde[CC] = Serdes.serdeFrom(serializer, deserializer)
+  implicit final def serde[CC: Encoder: Decoder]: Serde[CC] = Serdes.serdeFrom(serializer, deserializer)
 }
 
 object KafkaSerdesWithCirceSerdes extends Serdes with CirceSerdes
