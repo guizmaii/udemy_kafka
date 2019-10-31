f8b15b97f091e2052439bc1841728c936ddc2c21
jules Ivanic <jules.ivanic@gmail.com>
2019-11-01 09:21:31 +1100

    Update docker versions

--- 2 - Confluent Schema Registry & REST Proxy/2-start-kafka/confluent-tools.sh
+++ 2 - Confluent Schema Registry & REST Proxy/2-start-kafka/confluent-tools.sh
@@ -11,6 +11,6 @@ kafka-avro-console-consumer.sh
 # Option 2:
 # Use a docker image to have access to all the binaries right away:
 
-docker run -it --rm --net=host confluentinc/cp-schema-registry:3.3.1 bash
+docker run -it --rm --net=host confluentinc/cp-schema-registry:5.3.1 bash
 # Then you can do 
 kafka-avro-console-consumer
