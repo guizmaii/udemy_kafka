3b216d5fabe6332c7bf18ca8f229914ba141dbf4
jules Ivanic <jules.ivanic@gmail.com>
2019-10-31 12:51:37 +1100

    Improve test

--- 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/MainTests.scala
+++ 1 - Kafka Streams for Data Processing/4-bank-balance/src/test/scala/com/guizmaii/udemy/kafka/stream/bank/MainTests.scala
@@ -34,24 +34,24 @@ class MainTests extends FreeSpec with Matchers {
 
     "sums the, grouped by key, Messages amounts" in {
       testStream(topology) { (producer: Producer[String, Message], consumer: Consumer[String, Long]) =>
-        val key        = "key"
-        val anotherKey = "anotherKey"
-        val m_0        = Message(name = "Jules", amount = 2, time = Instant.MAX)
-        val m_1        = Message(name = "Jules", amount = 6, time = Instant.MAX)
-        val m_2        = Message(name = "Jules", amount = 3, time = Instant.MAX)
-        val m_3        = Message(name = "Jules", amount = 8, time = Instant.MAX)
+        val key_A = "key_A"
+        val key_B = "key_B"
+        val m_0   = Message(name = "Jules", amount = 2, time = Instant.MAX)
+        val m_1   = Message(name = "Jules", amount = 6, time = Instant.MAX)
+        val m_2   = Message(name = "Jules", amount = 3, time = Instant.MAX)
+        val m_3   = Message(name = "Jules", amount = 8, time = Instant.MAX)
 
-        producer.produce(sourceTopic)(key, m_0)
-        producer.produce(sourceTopic)(key, m_1)
-        producer.produce(sourceTopic)(anotherKey, m_2)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(key, m_1)
+        producer.produce(sourceTopic)(key_A, m_0)
+        producer.produce(sourceTopic)(key_A, m_1)
+        producer.produce(sourceTopic)(key_B, m_2)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_A, m_1)
 
-        consumer.consume(outputTopic).value() should be(m_0.amount)
-        consumer.consume(outputTopic).value() should be(m_0.amount + m_1.amount)
-        consumer.consume(outputTopic).value() should be(m_2.amount)
-        consumer.consume(outputTopic).value() should be(m_2.amount + m_3.amount)
-        consumer.consume(outputTopic).value() should be(m_0.amount + m_1.amount * 2)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> m_0.amount)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> (m_0.amount + m_1.amount))
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> m_2.amount)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> (m_2.amount + m_3.amount))
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> (m_0.amount + m_1.amount * 2))
         consumer.consume(outputTopic) should be(null) // Assert that I consumed all the messages
       }
     }
@@ -68,30 +68,30 @@ class MainTests extends FreeSpec with Matchers {
 
     "counts the number of transactions, grouped by key" in {
       testStream(topology) { (producer: Producer[String, Message], consumer: Consumer[String, Long]) =>
-        val key        = "key"
-        val anotherKey = "anotherKey"
-        val m_0        = Message(name = "Jules", amount = 2, time = Instant.MAX)
-        val m_1        = Message(name = "Jules", amount = 6, time = Instant.MAX)
-        val m_2        = Message(name = "Jules", amount = 3, time = Instant.MAX)
-        val m_3        = Message(name = "Jules", amount = 8, time = Instant.MAX)
+        val key_A = "key_A"
+        val key_B = "key_B"
+        val m_0   = Message(name = "Jules", amount = 2, time = Instant.MAX)
+        val m_1   = Message(name = "Jules", amount = 6, time = Instant.MAX)
+        val m_2   = Message(name = "Jules", amount = 3, time = Instant.MAX)
+        val m_3   = Message(name = "Jules", amount = 8, time = Instant.MAX)
 
-        producer.produce(sourceTopic)(key, m_0)
-        producer.produce(sourceTopic)(key, m_1)
-        producer.produce(sourceTopic)(anotherKey, m_2)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(key, m_1)
+        producer.produce(sourceTopic)(key_A, m_0)
+        producer.produce(sourceTopic)(key_A, m_1)
+        producer.produce(sourceTopic)(key_B, m_2)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_A, m_1)
 
-        consumer.consume(outputTopic).value() should be(1)
-        consumer.consume(outputTopic).value() should be(2)
-        consumer.consume(outputTopic).value() should be(1)
-        consumer.consume(outputTopic).value() should be(2)
-        consumer.consume(outputTopic).value() should be(3)
-        consumer.consume(outputTopic).value() should be(4)
-        consumer.consume(outputTopic).value() should be(5)
-        consumer.consume(outputTopic).value() should be(3)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> 1)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> 2)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> 1)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> 2)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> 3)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> 4)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> 5)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> 3)
         consumer.consume(outputTopic) should be(null) // Assert that I consumed all the messages
       }
     }
@@ -108,30 +108,30 @@ class MainTests extends FreeSpec with Matchers {
 
     "keep the latest transaction timestamp for a given key" in {
       testStream(topology) { (producer: Producer[String, Message], consumer: Consumer[String, Instant]) =>
-        val key        = "key"
-        val anotherKey = "anotherKey"
-        val m_0        = Message(name = "Jules", amount = 2, time = Instant.MIN)
-        val m_1        = Message(name = "Jules", amount = 6, time = Instant.MIN + 2.minutes)
-        val m_2        = Message(name = "Jules", amount = 3, time = Instant.MIN + 4.minutes)
-        val m_3        = Message(name = "Jules", amount = 8, time = Instant.MIN + 8.minutes)
+        val key_A = "key_A"
+        val key_B = "key_B"
+        val m_0   = Message(name = "Jules", amount = 2, time = Instant.MIN)
+        val m_1   = Message(name = "Jules", amount = 6, time = Instant.MIN + 2.minutes)
+        val m_2   = Message(name = "Jules", amount = 3, time = Instant.MIN + 4.minutes)
+        val m_3   = Message(name = "Jules", amount = 8, time = Instant.MIN + 8.minutes)
 
-        producer.produce(sourceTopic)(key, m_0)
-        producer.produce(sourceTopic)(key, m_1)
-        producer.produce(sourceTopic)(anotherKey, m_2)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_2)
-        producer.produce(sourceTopic)(key, m_0)
+        producer.produce(sourceTopic)(key_A, m_0)
+        producer.produce(sourceTopic)(key_A, m_1)
+        producer.produce(sourceTopic)(key_B, m_2)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_2)
+        producer.produce(sourceTopic)(key_A, m_0)
 
-        consumer.consume(outputTopic).value() should be(m_0.time)
-        consumer.consume(outputTopic).value() should be(m_1.time)
-        consumer.consume(outputTopic).value() should be(m_2.time)
-        consumer.consume(outputTopic).value() should be(m_3.time)
-        consumer.consume(outputTopic).value() should be(m_3.time)
-        consumer.consume(outputTopic).value() should be(m_3.time)
-        consumer.consume(outputTopic).value() should be(m_2.time)
-        consumer.consume(outputTopic).value() should be(m_0.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> m_0.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> m_1.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> m_2.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> m_3.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> m_3.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> m_3.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> m_2.time)
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> m_0.time)
         consumer.consume(outputTopic) should be(null) // Assert that I consumed all the messages
       }
     }
@@ -151,31 +151,31 @@ class MainTests extends FreeSpec with Matchers {
 
     "aggregates the results of the 3 other streams" in {
       testStream(topology) { (producer: Producer[String, Message], consumer: Consumer[String, FinalResult]) =>
-        val key        = "key"
-        val anotherKey = "anotherKey"
-        val m_0        = Message(name = "Jules", amount = 2, time = Instant.MIN)
-        val m_1        = Message(name = "Jules", amount = 6, time = Instant.MIN + 2.minutes)
-        val m_2        = Message(name = "Jules", amount = 3, time = Instant.MIN + 4.minutes)
-        val m_3        = Message(name = "Jules", amount = 8, time = Instant.MIN + 8.minutes)
+        val key_A = "key_A"
+        val key_B = "key_B"
+        val m_0   = Message(name = "Jules", amount = 2, time = Instant.MIN)
+        val m_1   = Message(name = "Jules", amount = 6, time = Instant.MIN + 2.minutes)
+        val m_2   = Message(name = "Jules", amount = 3, time = Instant.MIN + 4.minutes)
+        val m_3   = Message(name = "Jules", amount = 8, time = Instant.MIN + 8.minutes)
 
-        producer.produce(sourceTopic)(key, m_0)
-        producer.produce(sourceTopic)(key, m_1)
-        producer.produce(sourceTopic)(anotherKey, m_2)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_3)
-        producer.produce(sourceTopic)(anotherKey, m_2)
-        producer.produce(sourceTopic)(key, m_0)
+        producer.produce(sourceTopic)(key_A, m_0)
+        producer.produce(sourceTopic)(key_A, m_1)
+        producer.produce(sourceTopic)(key_B, m_2)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_3)
+        producer.produce(sourceTopic)(key_B, m_2)
+        producer.produce(sourceTopic)(key_A, m_0)
 
         // format: off
-        consumer.consume(outputTopic).keyAndValue should be(key         -> FinalResult(totalAmount = m_0.amount,                      transactionCount = 1, lastUpdated = m_0.time))
-        consumer.consume(outputTopic).keyAndValue should be(key         -> FinalResult(totalAmount = m_0.amount + m_1.amount,         transactionCount = 2, lastUpdated = m_1.time))
-        consumer.consume(outputTopic).keyAndValue should be(anotherKey  -> FinalResult(totalAmount = m_2.amount,                      transactionCount = 1, lastUpdated = m_2.time))
-        consumer.consume(outputTopic).keyAndValue should be(anotherKey  -> FinalResult(totalAmount = m_2.amount + m_3.amount,         transactionCount = 2, lastUpdated = m_3.time))
-        consumer.consume(outputTopic).keyAndValue should be(anotherKey  -> FinalResult(totalAmount = m_2.amount + m_3.amount * 2,     transactionCount = 3, lastUpdated = m_3.time))
-        consumer.consume(outputTopic).keyAndValue should be(anotherKey  -> FinalResult(totalAmount = m_2.amount + m_3.amount * 3,     transactionCount = 4, lastUpdated = m_3.time))
-        consumer.consume(outputTopic).keyAndValue should be(anotherKey  -> FinalResult(totalAmount = m_2.amount * 2 + m_3.amount * 3, transactionCount = 5, lastUpdated = m_2.time))
-        consumer.consume(outputTopic).keyAndValue should be(key         -> FinalResult(totalAmount = m_0.amount * 2 + m_1.amount,     transactionCount = 3, lastUpdated = m_0.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> FinalResult(totalAmount = m_0.amount,                      transactionCount = 1, lastUpdated = m_0.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> FinalResult(totalAmount = m_0.amount + m_1.amount,         transactionCount = 2, lastUpdated = m_1.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> FinalResult(totalAmount = m_2.amount,                      transactionCount = 1, lastUpdated = m_2.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> FinalResult(totalAmount = m_2.amount + m_3.amount,         transactionCount = 2, lastUpdated = m_3.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> FinalResult(totalAmount = m_2.amount + m_3.amount * 2,     transactionCount = 3, lastUpdated = m_3.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> FinalResult(totalAmount = m_2.amount + m_3.amount * 3,     transactionCount = 4, lastUpdated = m_3.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_B -> FinalResult(totalAmount = m_2.amount * 2 + m_3.amount * 3, transactionCount = 5, lastUpdated = m_2.time))
+        consumer.consume(outputTopic).keyAndValue should be(key_A -> FinalResult(totalAmount = m_0.amount * 2 + m_1.amount,     transactionCount = 3, lastUpdated = m_0.time))
         consumer.consume(outputTopic) should be(null) // Assert that I consumed all the messages
         // format: on
       }
