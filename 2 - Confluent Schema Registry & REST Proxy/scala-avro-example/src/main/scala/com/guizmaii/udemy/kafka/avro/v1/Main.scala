95a880d14b69a021b6e825e781c4834e0fe31653
jules Ivanic <jules.ivanic@gmail.com>
2019-11-01 16:35:13 +1100

    Write a simple Avro Kafka Consumer

--- 2 - Confluent Schema Registry & REST Proxy/scala-avro-example/src/main/scala/com/guizmaii/udemy/kafka/avro/v1/Main.scala
+++ 2 - Confluent Schema Registry & REST Proxy/scala-avro-example/src/main/scala/com/guizmaii/udemy/kafka/avro/v1/Main.scala
@@ -1,11 +1,13 @@
 package com.guizmaii.udemy.kafka.avro.v1
 
-import cats.effect.{ExitCode, IO, IOApp, Resource}
-import com.sksamuel.avro4s.{SchemaFor, ToRecord}
+import cats.effect.{ ExitCode, IO, IOApp, Resource }
+import com.banno.kafka.consumer.ConsumerApi
+import com.sksamuel.avro4s.{ RecordFormat, SchemaFor }
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import org.apache.kafka.clients.admin.NewTopic
-import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.{ ProducerRecord, RecordMetadata }
+import org.apache.kafka.common.TopicPartition
 
 object Main extends IOApp {
 
@@ -15,17 +17,21 @@ object Main extends IOApp {
   import com.banno.kafka.producer._
   import com.banno.kafka.schemaregistry._
 
-  implicit def asProducerRecord[K, V](t: (NewTopic, K, V)): ProducerRecord[K, V] =
-    new ProducerRecord[K, V](t._1.name, t._2, t._3)
+  import scala.concurrent.duration._
+
+  implicit final class ProducerOps[F[_], K, V](private val producer: ProducerApi[F, K, V]) extends AnyVal {
+    def sendAsync(topic: NewTopic, k: K, v: V): F[RecordMetadata] =
+      producer.sendAsync(new ProducerRecord[K, V](topic.name, k, v))
+  }
 
   final case class CustomerId(id: String)
   object CustomerId {
-    implicit final val record: ToRecord[CustomerId] = ToRecord[CustomerId]
+    implicit final val format: RecordFormat[CustomerId] = RecordFormat[CustomerId]
   }
 
   final case class Customer(firstName: String, lastName: String, age: Int, height: Float)
   object Customer {
-    implicit final val record: ToRecord[Customer] = ToRecord[Customer]
+    implicit final val format: RecordFormat[Customer] = RecordFormat[Customer]
   }
 
   val sourceTopic      = new NewTopic("customer-avro-topic", 1, 1)
@@ -36,10 +42,19 @@ object Main extends IOApp {
     ProducerApi.Avro4s
       .resource[IO, CustomerId, Customer](
         bootstrapServers,
+        registry,
         ClientId("customer-avro-producer"),
-        EnableIdempotence(true),
-        registry
+        EnableIdempotence(true)
       )
+
+  val consumerR: Resource[IO, ConsumerApi[IO, CustomerId, Customer]] =
+    ConsumerApi.Avro4s.resource[IO, CustomerId, Customer](
+      bootstrapServers,
+      registry,
+      ClientId("customer-avro-consumer"),
+      GroupId("customer-avro-consumer-group"),
+      AutoOffsetReset.earliest
+    )
 
   def registerSchema[K: SchemaFor, V: SchemaFor](registryUrl: SchemaRegistryUrl, topic: NewTopic): IO[(Int, Int)] =
     SchemaRegistryApi.register[IO, K, V](registryUrl.url, topic.name)
@@ -53,12 +68,21 @@ object Main extends IOApp {
         _                                                <- registerSchema[CustomerId, Customer](registry, sourceTopic)
         _ <- producerR.use { producer =>
               producer.sendAsync(
-                (
-                  sourceTopic,
-                  CustomerId("Jules"),
-                  Customer(firstName = "Jules", lastName = "Ivanic", age = 31, height = 176.2f)
-                )
+                sourceTopic,
+                CustomerId("Jules"),
+                Customer(firstName = "Jules", lastName = "Ivanic", age = 31, height = 176.2f)
               )
+            }
+        _ <- consumerR.use { consumer: ConsumerApi[IO, CustomerId, Customer] =>
+              val initialOffsets = Map.empty[TopicPartition, Long] // Start from beginning
+
+              consumer.assign(sourceTopic.name, initialOffsets) *>
+                consumer
+                  .recordStream(1.second)
+                  .take(1)
+                  .compile
+                  .toVector
+                  .map(_.foreach(println))
             }
       } yield "Done"
 
